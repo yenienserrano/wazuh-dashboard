72c6b5933abfcf2215262fcd7de2dfab54a47957
Ian Yenien Serrano <63758389+yenienserrano@users.noreply.github.com>
2025-05-23 22:38:20 +0200

    Fix tests

--- src/core/public/chrome/ui/header/collapsible_nav.test.tsx
+++ src/core/public/chrome/ui/header/collapsible_nav.test.tsx
@@ -200,11 +200,11 @@ describe('CollapsibleNav', () => {
     expectShownNavLinksCount(component, 3);
     clickGroup(component, 'explore');
     clickGroup(component, 'recentlyViewed');
-    expectShownNavLinksCount(component, 2);
+    expectShownNavLinksCount(component, 1);
     component.setProps({ isNavOpen: false });
     expectNavIsClosed(component);
     component.setProps({ isNavOpen: true });
-    expectShownNavLinksCount(component, 2);
+    expectShownNavLinksCount(component, 1);
   });
 
   it('closes the nav after clicking a link', () => {
